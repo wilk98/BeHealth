fd497d9f0c7703b113c2967dd81bf37ee4c95fd8
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-16 19:46:37 +0100

    create client component

---
+++ BeHealth/Frontend/BeHealthFrontend/src/components/Footer/Client.tsx
@@ -0,0 +1,13 @@
+function Client() {
+	return (
+		<div className="client">
+			<p className="title">Klient</p>
+			<p>Cennik usług</p>
+			<p>Kategorie</p>
+			<p>Prześlij opinię</p>
+			<p>Umów wizytę</p>
+		</div>
+	);
+}
+
+export default Client;
