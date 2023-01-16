f28814467925ce37c4ed296caefec808a4505675
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-16 19:45:50 +0100

    add export default

--- BeHealth/Frontend/BeHealthFrontend/src/components/Logo.tsx
+++ BeHealth/Frontend/BeHealthFrontend/src/components/Logo.tsx
@@ -1,11 +1,15 @@
-import './Logo.css'
+import "./Logo.css";
 
 interface Logo {
-    fontSize?: number;
+	fontSize?: number;
 }
 
 export const Logo = ({ fontSize = 20 }: Logo) => {
-  return (
-    <p style={{fontSize: fontSize}}><b className='purple'>Be</b>Health</p>
-  )
-}
+	return (
+		<p style={{ fontSize: fontSize }}>
+			<b className="purple">Be</b>Health
+		</p>
+	);
+};
+
+export default Logo;
