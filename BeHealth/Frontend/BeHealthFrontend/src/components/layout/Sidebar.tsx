d31ea78fd2f4cda745057e04abf7fef3c2640884
Andrii Danylkov <danilkovxp@gmail.com>
2023-01-17 12:26:15 +0100

    add sidebar

--- BeHealth/Frontend/BeHealthFrontend/src/components/layout/Sidebar.tsx
+++ BeHealth/Frontend/BeHealthFrontend/src/components/layout/Sidebar.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import './Sidebar.css'
+import {  FaRegCalendar, FaRegClipboard, FaRegUser, IoSettingsSharp, FiMessageSquare, GiHealthNormal } from 'react-icons/all'
+import { useState } from 'react'
 
 export const Sidebar = () => {
+  const [selected, setSelected] = useState(0)
+
+  const links = ["Kalendarz" , "Nadchodzące wizyty", "Profil", "Wiadomości", "Pacjenci", "Ustawienia"]
+  const icons = [<FaRegCalendar />, <FaRegClipboard />, <FaRegUser />, <FiMessageSquare />, <GiHealthNormal />, <IoSettingsSharp />  ]
+
+  const linkClass = (selected:number, i: number, collectionLength: number) => {
+    let className = selected === i ? 'selected ' : ''
+    className += i === collectionLength - 1 ? 'last-item' : ''
+    return className;
+  }
+
+  const linkElements = links.map((link, i) => (
+    <li onClick={() => setSelected(i)}  className={linkClass(selected, i, links.length)}>{icons[i]} {link}</li>
+  ))
+
   return (
-    <aside></aside>
+    <aside>
+      <ul>
+        {linkElements}
+      </ul>
+    </aside>
   )
 }
