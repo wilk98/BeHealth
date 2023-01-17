5a5d6bd2578c2c3fc37cd2e3d016c9da43b9e87b
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-17 20:55:39 +0100

    create fluent API configuration for visits

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Entities/Configurations/VisitConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BeHealthBackend.Entities.Configurations
+{
+    public class VisitConfiguration : IEntityTypeConfiguration<Visit>
+    {
+        public void Configure(EntityTypeBuilder<Visit> builder)
+        {
+            builder
+                .Property(x => x.VisitDate).HasDefaultValueSql("getutcdate()");
+        }
+    }
+}
