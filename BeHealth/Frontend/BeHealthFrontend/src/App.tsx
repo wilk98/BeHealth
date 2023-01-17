35ad957c3a6eb0631ed436f2fad2d68db715ad7f
Andrii Danylkov <danilkovxp@gmail.com>
2023-01-17 10:24:24 +0100

    add navbar

--- BeHealth/Frontend/BeHealthFrontend/src/App.tsx
+++ BeHealth/Frontend/BeHealthFrontend/src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Footer from "./components/layout/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
 import ArangeVisit from "./pages/ArangeVisit";
+import { Navbar } from "./components/layout/Navbar";
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Routes>
 				<Route path="/arange-visit" element={<ArangeVisit />} />
 			</Routes>
