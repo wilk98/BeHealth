74ff52ccb06435755240177b01f1c6de63301940
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-17 20:23:05 +0100

    add database configuration

--- BeHealth/Backend/BeHealthBackend/BeHealthBackend/Program.cs
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Program.cs
@@ -1,15 +1,18 @@
+using BeHealthBackend.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<BeHealthContext>(
+    option => option
+        .UseSqlServer(builder.Configuration.GetConnectionString("BeHeathConnectionString")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
