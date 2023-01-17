e0e7ceb779455104beaeb9dfaa408730c97839bc
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-17 21:41:42 +0100

    create fluent API configuration for clinic

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Entities/Configurations/ClinicConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BeHealthBackend.Entities.Configurations
+{
+    public class ClinicConfiguration : IEntityTypeConfiguration<Clinic>
+    {
+        public void Configure(EntityTypeBuilder<Clinic> builder)
+        {
+        }
+    }
+}
