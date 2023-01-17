41115b81358079a790ae95f7cd2c495fc2211b44
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-17 21:08:41 +0100

    create fluent API configuration for patients

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Entities/Configurations/PatientConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeHealthBackend.Entities.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder
+                .Property(p => p.Created).HasDefaultValueSql("getutcdate()");
+        }
+    }
+}
