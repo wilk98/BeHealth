efca061f33d7f4fe80d60cbe77690c11916de7fb
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-17 21:42:17 +0100

    create fluent API configuration for address with relationship setup

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Entities/Configurations/AddressConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BeHealthBackend.Entities.Configurations
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasOne(p => p.Person)
+                .WithOne(a => a.Address)
+                .HasForeignKey<Person>(p => p.AddressId);
+
+            builder.HasOne(c => c.Clinic)
+                .WithOne(a => a.Address)
+                .HasForeignKey<Clinic>(c => c.AddressId);
+        }
+    }
+}
