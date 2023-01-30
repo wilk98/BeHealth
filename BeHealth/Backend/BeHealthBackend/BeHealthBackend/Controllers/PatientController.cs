d44521c53624412d8297404f61c8731310f7ba1b
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-30 12:02:12 +0100

    create patient controller

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Controllers/PatientController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BeHealthBackend.DataAccess.Repositories.Interfaces;
+using BeHealthBackend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeHealthBackend.Controllers;
+
+[ApiController, Route("/api/patients")]
+public class PatientController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public PatientController(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var patients = await _unitOfWork.PatientRepository
+            .GetAllAsync();
+
+        var patientsDtos = _mapper.Map<List<PatientDto>>(patients);
+
+        return Ok(patientsDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDoctor([FromRoute] int id)
+    {
+        var patient = await _unitOfWork.PatientRepository
+            .GetAsync(id);
+
+        if (patient is null) return NotFound();
+
+        var patientDto = _mapper.Map<PatientDto>(patient);
+
+        return Ok(patientDto);
+    }
+}
