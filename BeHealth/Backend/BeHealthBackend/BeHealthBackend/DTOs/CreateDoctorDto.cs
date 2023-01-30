fe13d478701c687f786f205c2667b5fce3424d0b
Ainsik <pio.andrzejczuk@gmail.com>
2023-01-30 15:09:38 +0100

    create doctor dto

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/DTOs/CreateDoctorDto.cs
@@ -0,0 +1,16 @@
+﻿using BeHealthBackend.DataAccess.Entities;
+
+namespace BeHealthBackend.DTOs
+{
+    public class CreateDoctorDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public Specialist Specialist { get; set; }
+    }
+}
