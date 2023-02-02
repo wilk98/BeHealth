54e1ef13843699213a3f4f0c2fe856b638bf6378
Ainsik <pio.andrzejczuk@gmail.com>
2023-02-02 12:06:01 +0100

    language changed

--- BeHealth/Frontend/BeHealthFrontend/src/components/layout/Navbar.tsx
+++ BeHealth/Frontend/BeHealthFrontend/src/components/layout/Navbar.tsx
@@ -1,52 +1,66 @@
-import { Logo } from "./Logo"
-import { PrimaryButton } from "../ui/PrimaryButton"
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
-import './Navbar.css'
-import { useState } from "react"
+import { Logo } from "./Logo";
+import { PrimaryButton } from "../ui/PrimaryButton";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./Navbar.css";
+import { useState } from "react";
 
 const Menu = () => {
-  return (
-    <ul>
-      <li><a href="">About</a></li>
-      <li><a href="">Categories</a></li>
-      <li><a href="">Reviews</a></li>
-      <li><a href="">Price</a></li>
-      <li><a href="">Contact</a></li>
-    </ul>
-  )
-}
-
+	return (
+		<ul>
+			<li>
+				<a href="">O nas</a>
+			</li>
+			<li>
+				<a href="">Kategorie</a>
+			</li>
+			<li>
+				<a href="">Recenzje</a>
+			</li>
+			<li>
+				<a href="">Cennik</a>
+			</li>
+			<li>
+				<a href="">Kontakt</a>
+			</li>
+		</ul>
+	);
+};
 
 export const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false)
+	const [toggleMenu, setToggleMenu] = useState(false);
 
-  return (
-    <nav>
-      <a href=""><Logo fontSize={20} /></a>
-      <div className="nav_container">
-        <Menu />
-        <div className="login-links">
-          <a href="">Login</a>
-          <PrimaryButton>
-            <a href="">Sign Up</a>
-          </PrimaryButton>
-        </div>
-      </div>
-      <div className="navbar-menu ">
-        {toggleMenu ? <RiCloseLine size={36} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line size={36} onClick={() => setToggleMenu(true)} />}
-        {toggleMenu && (
-          <div className="navbar-menu_container drop-down">
-            <Menu />
-            <div className="login-links">
-              <a href="">Login</a>
-              <PrimaryButton>
-                <a href="">Sign Up</a>
-              </PrimaryButton>
-            </div>
-          </div>
-        )}
-      </div>
-    </nav>
-  )
-}
+	return (
+		<nav>
+			<a href="">
+				<Logo fontSize={20} />
+			</a>
+			<div className="nav_container">
+				<Menu />
+				<div className="login-links">
+					<a href="">Login</a>
+					<PrimaryButton>
+						<a href="">Rejestracja</a>
+					</PrimaryButton>
+				</div>
+			</div>
+			<div className="navbar-menu ">
+				{toggleMenu ? (
+					<RiCloseLine size={36} onClick={() => setToggleMenu(false)} />
+				) : (
+					<RiMenu3Line size={36} onClick={() => setToggleMenu(true)} />
+				)}
+				{toggleMenu && (
+					<div className="navbar-menu_container drop-down">
+						<Menu />
+						<div className="login-links">
+							<a href="">Login</a>
+							<PrimaryButton>
+								<a href="">Rejestracja</a>
+							</PrimaryButton>
+						</div>
+					</div>
+				)}
+			</div>
+		</nav>
+	);
+};
