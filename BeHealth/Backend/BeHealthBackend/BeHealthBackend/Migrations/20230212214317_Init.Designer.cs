22570c04c9dd665ba61301c6fcccd8dd8ef779f1
Ainsik <pio.andrzejczuk@gmail.com>
2023-02-12 22:45:19 +0100

    change migration

--- BeHealth/Backend/BeHealthBackend/BeHealthBackend/Migrations/20230212214317_Init.Designer.cs
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Migrations/20230212214317_Init.Designer.cs
@@ -4,6 +4,7 @@ using BeHealthBackend.DataAccess.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeHealthBackend.Migrations
 {
     [DbContext(typeof(BeHealthContext))]
-    partial class BeHealthContextModelSnapshot : ModelSnapshot
+    [Migration("20230212214317_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
