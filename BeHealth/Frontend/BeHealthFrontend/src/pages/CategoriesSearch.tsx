ae8c87face4a8aab8b4f8ae9e7f688398374daff
wilk98 <maciekwilk09@gmail.com>
2023-02-13 12:19:39 +0100

    create page with searcher and slider

--- BeHealth/Frontend/BeHealthFrontend/src/pages/CategoriesSearch.tsx
+++ BeHealth/Frontend/BeHealthFrontend/src/pages/CategoriesSearch.tsx
@@ -1,10 +1,69 @@
-import Searcher from "../components/layout/Searcher/Searcher";
-import CategorySlider from "../components/layout/Searcher/CategorySlider";
+import Carousel from "../components/layout/Searcher/Carousel"
+import Searcher from "../components/layout/Searcher/Searcher"
+import picture from "../assets/images/specialization-rectangle.png"
+
+
 
 function CategoriesSearch() {
+	var actualWidth = document.querySelector('main')?.offsetWidth;
+    if (actualWidth != null)
+    {
+        var itemOnPage = Math.floor(actualWidth/170);
+        console.log(itemOnPage);
+    }
+    else
+    {
+        itemOnPage = 0;
+    }
 	return (
-			<CategorySlider/>
-	);
+		<main >
+			
+			<div className="p" style={{ marginLeft: 20 }}>
+			<Searcher />
+				<Carousel show={itemOnPage}>
+					<div className="picture" style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture" style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+					<div className="picture"style={{width:"150px"}}>
+						<img src={picture}alt="placeholder" style={{ width: '150px', height: "150px", padding: 8}} />
+					</div>
+				</Carousel>
+			</div>
+		</main>
+	)
 }
 
-export default CategoriesSearch;
+export default CategoriesSearch
