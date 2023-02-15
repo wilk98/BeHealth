1b18096aaad9c126d659e97ac2c27042ee59cfcc
wilk98 <maciekwilk09@gmail.com>
2023-02-15 03:10:42 +0100

    delete old css files and class

--- BeHealth/Frontend/BeHealthFrontend/src/components/layout/Searcher/Categories.tsx
+++
@@ -1,9 +0,0 @@
-function Categories() {
-	return (
-		<div className="categories">
-			<span className="categories_text">Kategorie</span>
-		</div>
-	);
-}
-
-export default Categories;
--- BeHealth/Frontend/BeHealthFrontend/src/components/layout/Searcher/Search.tsx
+++
@@ -1,9 +0,0 @@
-function Search() {
-	return (
-		<div className="s">
-			<input className="search" placeholder="Szukaj"/>
-		</div>
-	);
-}
-
-export default Search;
