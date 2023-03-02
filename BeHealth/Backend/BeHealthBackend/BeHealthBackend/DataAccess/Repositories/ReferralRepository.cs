d92944323b93db41be6fb7c1c27063463b4eabec
wilk98 <maciekwilk09@gmail.com>
2023-03-02 18:59:40 +0100

    Add Referral Repository

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/DataAccess/Repositories/ReferralRepository.cs
@@ -0,0 +1,14 @@
+﻿using BeHealthBackend.DataAccess.DbContexts;
+using BeHealthBackend.DataAccess.Entities;
+using BeHealthBackend.DataAccess.Repositories.Interfaces;
+
+namespace BeHealthBackend.DataAccess.Repositories;
+
+public class ReferralRepository : Repository<Referral>, IReferralRepository
+{
+    public ReferralRepository(BeHealthContext context) : base(context)
+    {
+        DbSet = context.Referrals;
+    }
+}
+
