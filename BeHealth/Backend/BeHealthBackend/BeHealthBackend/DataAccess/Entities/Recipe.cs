61693fca47e3515292074fe900d94f4d314fb5da
wilk98 <maciekwilk09@gmail.com>
2023-03-02 18:55:14 +0100

    add model of Recipe

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/DataAccess/Entities/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace BeHealthBackend.DataAccess.Entities
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public Patient? Patient { get; set; }
+        public DateTime Created { get; set; }
+        public string? Medicament { get; set; }
+        public int? Quantity { get; set; }
+        public string? Code { get; set; }
+    }
+}
