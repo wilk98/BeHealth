195814fe14d1ac0d956636bc91d6d3dd3585d5a1
wilk98 <maciekwilk09@gmail.com>
2023-03-02 19:02:34 +0100

    Add Recipe Controller

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using BeHealthBackend.DTOs.RecipeDtoFolder;
+using BeHealthBackend.Services.RecipeService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeHealthBackend.Controllers;
+
+[ApiController, Route("/api/recipes")]
+//[Authorize(Roles = "Patient")]
+public class RecipeController : ControllerBase
+{
+    private readonly IRecipeService _recipeService;
+
+    public RecipeController(IRecipeService recipeService)
+    {
+        _recipeService = recipeService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<IEnumerable<RecipeDto>>> GetRecipesByIdAsync([FromRoute] int id)
+    {
+        var recipes = await _recipeService.GetIdAsync(id);
+        return Ok(recipes);
+    }
+
+}
