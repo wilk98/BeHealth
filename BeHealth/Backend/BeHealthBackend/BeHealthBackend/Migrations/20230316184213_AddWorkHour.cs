85fd494f71e82f1d5184c75a9013ed4c45dc5209
wilk98 <maciekwilk09@gmail.com>
2023-03-17 00:31:13 +0100

    add migration to database with Work Hours

---
+++ BeHealth/Backend/BeHealthBackend/BeHealthBackend/Migrations/20230316184213_AddWorkHour.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeHealthBackend.Migrations
+{
+    public partial class AddWorkHour : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkHours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    Day = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartHour = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EndHour = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkHours", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkHours_Persons_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkHours_DoctorId",
+                table: "WorkHours",
+                column: "DoctorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkHours");
+        }
+    }
+}
